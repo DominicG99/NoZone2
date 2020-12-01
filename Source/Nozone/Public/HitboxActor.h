0d961b4ee766cb82bfff05dfd26248d485913e79
Dom <dominicginter2017@gmail.com>
2020-11-30 21:57:25 -0800

    Hitbox added and hard code obliterated

---
+++ Source/Nozone/Public/HitboxActor.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HitboxActor.generated.h"
+
+UENUM(BlueprintType)
+enum class EHitboxEnum : uint8
+{
+	HB_STRIKE	UMETA(DisplayName = "Strike"),
+	HB_HURTBOX	UMETA(DisplayName = "Hurtbox")
+};
+
+
+UCLASS()
+class NOZONE_API AHitboxActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHitboxActor();
+
+protected:
+
+	//Damage of HB
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hitbox")
+		int hitboxDamage;
+
+	// Location of HB
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hitbox")
+		FVector hitboxLocation;
+
+	//Type of HB
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hitbox")
+		EHitboxEnum hitboxType;
+
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
